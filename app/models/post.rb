aa815469672ade8d357593a9ca4a7825ec70c9de
Taylor Massey <tamassey92@gmail.com>
2022-06-08 17:28:50 -0500

    commit tree 41645161dcf88f3425a47c50c64f9d50d28eba9e

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :content, length: { maximum: 250 }
+     validates :category, inclusion { in: %w(Fiction Non-Fiction) }
+     
+     def must_include_in_title
+        unless title.match?(Wont Believe Secret Top [number] Guess)
+          errors.add(:title, "Error"
+        end
+      end
 end
